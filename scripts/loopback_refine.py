250d9974d02a3e08b6047e55b39b7760c522b430
David Marx <david.marx84@gmail.com>
2023-07-01 10:46:56 -0700

    Create loopback_refine.py

---
+++ scripts/loopback_refine.py
@@ -0,0 +1,10 @@
+# MVP:
+# 1. script should embed into text2video extension interface
+# 2. add a drop down specifying the "refinement" model (i.e. the upscaler)
+# 3. box for specifying the number of loopbacks (in future, maybe this could be inferred from strength curve?)
+
+# FUTURE:
+# 4. add a box for specifying prompts for refinement runs. todo: prompt-to-prompt syntax support
+# 5. add a box for specifying parameter curves. MVP: diffusion strengths
+
+# ... oh hmm.. looks like a1111 scripts are only supported for two builtin tabs
